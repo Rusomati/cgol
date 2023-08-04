3c2430317ed46c88ae19fa59faae73587df0dd1a
Rusomati <mangenthegamer@gmail.com>
2023-08-04 12:29:04 +0000

    added a basic way to draw cells

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <string.h>
 
 int screenWidth = 800;
 int screenHeight = 450;
@@ -12,19 +13,33 @@ void DrawTextC(char *text, float x, float y, int size, Color color)//draws cente
 
 int main(void)
 {
-
+	int cellsize = 20;
+	int borderWidth = cellsize/20;
+	int gridsize = (int)(screenWidth/cellsize)*(int)((screenHeight*0.9)/cellsize);
+	char grid[gridsize];
+	memset(grid,0,gridsize*sizeof(char));
 	InitWindow(screenWidth, screenHeight, "cgol");
 
 	SetTargetFPS(60);               // must be set to one at simulation or smth
+	
+	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT));
+	if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT));
+
 	while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
 
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT));
-		if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT));
 
 		BeginDrawing();
 
 		ClearBackground(GRAY);
+
+		for(int x,y,i=0;i<gridsize;i++)
+		{
+				x = cellsize * (i%(screenWidth/cellsize)) + borderWidth;
+				y = cellsize * (i/(screenWidth/cellsize)) + borderWidth;
+				if (!(grid[i])) DrawRectangle(x,y,cellsize-(2*borderWidth),cellsize-(2*borderWidth),BLACK);
+				else;
+		}
 
 		DrawTextC("(paused)", 0.5, 0.9, screenWidth/20, RED);
 
